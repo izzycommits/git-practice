fe92d2041a9a90ddc3f449a220b70b2e339f1dd3
Aleida Vieyra <avieyra@ufl.edu>
2024-09-23 17:51:34 -0400

    changes to function_d.py by Aleida V.

--- function_d.py
+++ function_d.py
@@ -3,6 +3,12 @@ def max_value(numbers):
         in the list.
     """
 
+    current_max = numbers[0]
+    for num in numbers:
+        if numbers[num] > current_max:
+            current_max = numbers[num]
+            
+    return current_max
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
